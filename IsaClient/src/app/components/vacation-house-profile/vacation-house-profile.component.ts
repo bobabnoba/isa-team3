e9b6dc2a07525614db49aab530f66d5e4c392261
momoirox <majablgic505@gmail.com>
2021-12-09 17:55:54 +0100

    Updated Frontend Client

---
+++ IsaClient/src/app/components/vacation-house-profile/vacation-house-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vacation-house-profile',
+  templateUrl: './vacation-house-profile.component.html',
+  styleUrls: ['./vacation-house-profile.component.css']
+})
+export class VacationHouseProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
