7b6506a01190057fffb0d0bda337484fb03203a6
Boba <bobaruljic@gmail.com>
2022-08-03 19:55:48 +0200

    adventure model updated

--- fishing-booker/src/main/java/com/ftn/fishingbooker/enumeration/FishingEquipment.java
+++
@@ -1,7 +0,0 @@
-package com.ftn.fishingbooker.enumeration;
-
-public enum FishingEquipment {
-    FISHING_ROD,
-    WORMS,
-    HOOKS
-}
---
+++ fishing-booker/src/main/java/com/ftn/fishingbooker/model/FishingEquipment.java
@@ -0,0 +1,35 @@
+package com.ftn.fishingbooker.model;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class FishingEquipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        FishingEquipment that = (FishingEquipment) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
