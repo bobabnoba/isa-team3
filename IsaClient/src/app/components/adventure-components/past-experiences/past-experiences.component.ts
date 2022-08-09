49e6c3f30852c23b1c7165e13f69e44993a4f227
Boba <bobaruljic@gmail.com>
2022-08-10 01:17:30 +0200

    carousel fixed

--- IsaClient/src/app/components/adventure-components/past-experiences/past-experiences.component.ts
+++ IsaClient/src/app/components/adventure-components/past-experiences/past-experiences.component.ts
@@ -1,23 +1,22 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Adventure } from 'src/app/interfaces/adventure';
 import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-past-experiences',
   templateUrl: './past-experiences.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./past-experiences.component.css']
 })
 export class PastExperiencesComponent implements OnInit {
 
   @Input()
   adventure! : Adventure;
-  img : string = ""
   baseUrl = environment.apiURL
+
   constructor() { }
 
   ngOnInit(): void {
-    console.log(this.adventure)
-    this.img = environment.apiURL + "/" + this.adventure.images[0]
     }
 
 }
