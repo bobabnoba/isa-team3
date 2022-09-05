8ae38f281055b2b78ae9bc4da559cafdc3e6dca2
Boba <bobaruljic@gmail.com>
2022-09-05 15:22:20 +0200

    authGuard

---
+++ IsaClient/src/app/AuthGuard/AuthGuardClient.ts
@@ -0,0 +1,22 @@
+﻿import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import jwt_decode from "jwt-decode";
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuardClient implements CanActivate {
+
+    constructor(private _router: Router) { }
+
+    canActivate() {
+        const token = localStorage.getItem("token")
+        if (token != null) {
+            if(localStorage.getItem("role") == "ROLE_CLIENT")
+                return true;
+        }
+
+        this._router.navigate(['/403']);
+        return false;
+    }
+}
+
